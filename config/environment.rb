dd285a8729ec64cc36da4555d78e5cb3e0b69665
Jeff Schoolcraft <jeff@thequeue.net>
2009-07-09 12:04:36 -0400

    dropped haml version to 2.0 so that run code run will actually work

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'haml', :version => '>= 2.2'
+  config.gem 'haml', :version => '~> 2.0'
   config.gem 'jnunemaker-twitter', :lib => 'twitter', :version => '>= 0.6.12'
   config.time_zone = 'UTC'
 end
