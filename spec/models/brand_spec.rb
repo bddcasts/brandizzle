1e8b2b1df36e24810648a9ba7da0e4facc0974d2
Jeff Schoolcraft <jeff@thequeue.net>
2009-07-21 12:33:58 -0400

    fixing warnings

--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Brand do
-  it { should have_column :name, :type => :string }
-  it { should validate_presence_of :name }
-  it { should have_many :searches }
+  it { should have_column(:name, :type => :string) }
+  it { should validate_presence_of(:name) }
+  it { should have_many(:searches) }
 end
