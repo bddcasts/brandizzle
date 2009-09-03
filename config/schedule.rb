d263a1debe8be532b2aaccaa9ad227bb7c7f0b1a
Jeff Schoolcraft <jeff@thequeue.net>
2009-09-03 10:10:45 -0400

    trying to get whenever schedule right for demo

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,11 @@
-every :hour do
+every 1.hour, :at => 15 do
   rake "brandizzle:reset_demo"
+end
+
+every 1.hour, :at => 45 do
+  rake "brandizzle:reset_demo"
+end
+
+every 30.minutes do
   runner "Search.run"
 end
