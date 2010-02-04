c615cc7ffc23af60fd1dcf9d51c6d4466180d024
Cristi Duma <cristi.duma@gmail.com>
2010-02-04 11:45:59 +0200

    updated cron schedule to not clean up the database every half hour

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,10 @@
-every 1.hour, :at => 15 do
-  rake "brandizzle:reset_demo"
-end
-
-every 1.hour, :at => 45 do
-  rake "brandizzle:reset_demo"
-end
+# every 1.hour, :at => 15 do
+#   rake "brandizzle:reset_demo"
+# end
+# 
+# every 1.hour, :at => 45 do
+#   rake "brandizzle:reset_demo"
+# end
 
 every 1.hour, :at => 17 do
   runner "Search.run"
