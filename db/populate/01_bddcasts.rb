832fc2639cfe976ac164f25ee4855a3ba8725835
Cristi Duma <cristi.duma@gmail.com>
2010-02-04 13:13:16 +0200

    populate users

--- db/populate/01_bddcasts.rb
+++ db/populate/01_bddcasts.rb
@@ -1,3 +1,12 @@
+istvan = User.create_or_update(:id => 1, :login => "ihoka", :email => "ihoka@brandizzle.com", :password => "secret", :password_confirmation => "secret")
+istvan.activate!
+
+jeff = User.create_or_update(:id => 2, :login => "jschoolcraft", :email => "jschoolcraft@brandizzle.com", :password => "secret", :password_confirmation => "secret")
+jeff.activate!
+
+cristi = User.create_or_update(:id => 3, :login => "cristi", :email => "cristi@brandizzle.com", :password => "secret", :password_confirmation => "secret")
+cristi.activate!
+
 bddcasts = Brand.create_or_update(:id => 1, :name => 'BDDCasts')
 
 bddcasts.add_search('bddcasts')
