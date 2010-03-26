ddee48e31c078002bf58fc40eaecc0c2b8e7cbd2
Cristi Duma <cristi.duma@gmail.com>
2010-03-26 17:43:29 +0200

    migration to added indexes on users table; added index on email and perishable_token

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100209140532) do
+ActiveRecord::Schema.define(:version => 20100326153915) do
 
   create_table "brand_queries", :force => true do |t|
     t.integer "brand_id"
@@ -87,17 +87,20 @@ ActiveRecord::Schema.define(:version => 20100209140532) do
   add_index "search_results", ["result_id"], :name => "index_search_results_on_result_id"
 
   create_table "users", :force => true do |t|
-    t.string   "login",                                :null => false
-    t.string   "email",                                :null => false
-    t.string   "crypted_password",                     :null => false
-    t.string   "password_salt",                        :null => false
-    t.string   "persistence_token",                    :null => false
-    t.string   "perishable_token",                     :null => false
-    t.boolean  "active",            :default => false, :null => false
+    t.string   "login",                               :null => false
+    t.string   "email",                               :null => false
+    t.string   "crypted_password",                    :null => false
+    t.string   "password_salt",                       :null => false
+    t.string   "persistence_token",                   :null => false
+    t.string   "perishable_token",                    :null => false
+    t.boolean  "active",            :default => true, :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "invitation_id"
     t.integer  "invitation_limit",  :default => 0
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email"
+  add_index "users", ["perishable_token"], :name => "index_users_on_perishable_token"
 
 end
