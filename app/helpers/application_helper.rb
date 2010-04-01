668507aa0afb219edd7f2d0234fe822c7900c6c4
Cristi Duma <cristi.duma@gmail.com>
2010-04-01 09:23:48 +0300

    finished time filter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,33 @@ module ApplicationHelper
       controller.controller_name == "teams" || controller.controller_name == "users"
     end
   end
+  
+  def brand_filter(label, brand_id=nil)
+    url_options = {:search => (params[:search] || {}).merge({:brand_id_is => brand_id})}
+    
+    link_to label, brand_results_path(url_options), :class => params[:search] && params[:search].try(:[], :brand_id_is) == brand_id.to_s && "selected" || nil
+  end
+
+  def time_filter(search, options={}, html_options={})
+    options[:before_scope] ||= "#{options[:by]}_before"
+    options[:after_scope] ||= "#{options[:by]}_after"
+    
+    after = options[:between].blank? ? Time.utc(1970, 1, 1).to_s(:db) : options[:between].first.to_time.to_s(:db)
+    before = options[:between].blank? ? Time.now.end_of_day.to_s(:db) : options[:between].last.to_time.to_s(:db)
+    
+    scope = { options[:before_scope] => before, options[:after_scope] => after }
+    
+    time_conditions = search.conditions.values.map{ |v| v.is_a?(Time) && v.to_s(:db)}
+    selected = time_conditions.include?(before) && time_conditions.include?(after)
+        
+    if selected
+      html_options[:class] = "selected"
+    end
+    
+    url_options = {
+      :search => (params[:search] || {}).merge(scope)
+    }
+    
+    link_to options[:as], url_for(url_options), html_options
+  end  
 end
