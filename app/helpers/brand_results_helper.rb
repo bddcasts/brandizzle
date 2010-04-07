424b7d8ce7329fbfd21769179ba02592078ac59c
Cristi Duma <cristi.duma@gmail.com>
2010-04-07 19:55:42 +0300

    modified brand_results index to group results by day and tag follow_up results

--- app/helpers/brand_results_helper.rb
+++ app/helpers/brand_results_helper.rb
@@ -2,4 +2,14 @@ module BrandResultsHelper
   def present_brand_result(brand_result)
     BrandResultPresenter.new(:brand_result => brand_result)
   end
+  
+  def show_day(day)
+    if day.today?
+      "Today"
+    elsif day.year == Time.now.year
+      day.strftime('%B %d')
+    else
+      day.strftime('%B %d, %Y')
+    end
+  end
 end
