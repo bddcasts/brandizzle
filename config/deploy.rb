e341e20e98480d2cf4a3a2732b64797e6d1c3a05
Istvan Hoka <istvan.hoka@gmail.com>
2010-06-19 14:45:30 +0300

    Run whenever with bundler

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,7 @@ namespace :deploy do
   
   desc "Update the crontab file"
   task :update_crontab, :roles => :db do
-    run "cd #{release_path} && whenever --update-crontab #{application} --set environment=#{rails_env}"
+    run "cd #{release_path} && bundle exec whenever --update-crontab #{application} --set environment=#{rails_env}"
   end
 end
 
